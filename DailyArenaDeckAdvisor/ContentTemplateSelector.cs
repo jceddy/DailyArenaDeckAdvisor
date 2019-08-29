9e42039297834c86bb816b8b140339052c64efa8
Joseph Eddy <jceddy@hotmail.com>
2019-08-29 08:07:31 -0500

    Added code comments to ContentTemplateSelector.

--- DailyArenaDeckAdvisor/ContentTemplateSelector.cs
+++ DailyArenaDeckAdvisor/ContentTemplateSelector.cs
@@ -1,15 +1,19 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
+﻿using System.Windows;
 using System.Windows.Controls;
 
 namespace DailyArenaDeckAdvisor
 {
+	/// <summary>
+	/// Class to select a Xaml template for the archetype tab detail view, depending on whether it's showing a deck, or a meta report.
+	/// </summary>
 	public class ContentTemplateSelector : DataTemplateSelector
 	{
+		/// <summary>
+		/// Select the template to use depending on the item type.
+		/// </summary>
+		/// <param name="item">The item being shown in the detail view.</param>
+		/// <param name="container">The container the view is in (not used).</param>
+		/// <returns>ArchetypeContentTemplate if the item is an Archetype, otherwise MetaReportContentTemplate.</returns>
 		public override DataTemplate SelectTemplate(object item, DependencyObject container)
 		{
 			Window win = Application.Current.MainWindow;
