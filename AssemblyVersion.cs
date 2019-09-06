8c329e6c7fb4e0920292587c764c1b022d7149e8
Joseph Eddy <jceddy@hotmail.com>
2019-09-06 08:56:00 -0500

    Added some code comments and made some developer QOL changes in the MetaReport code. Changed version to 1.0.3.1.

--- AssemblyVersion.cs
+++ AssemblyVersion.cs
@@ -12,6 +12,6 @@ using System.Reflection;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 
-[assembly: AssemblyVersion("1.0.3.0")]
-[assembly: AssemblyFileVersion("1.0.3.0")]
+[assembly: AssemblyVersion("1.0.3.1")]
+[assembly: AssemblyFileVersion("1.0.3.1")]
 [assembly: AssemblyCopyright("© 2019 DailyArena")]
--- DailyArenaDeckAdvisor/MetaReport.cs
+++ DailyArenaDeckAdvisor/MetaReport.cs
@@ -3,26 +3,84 @@ using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace DailyArenaDeckAdvisor
 {
+	/// <summary>
+	/// Class that represents a "Meta Report" for a given format.
+	/// </summary>
 	public class MetaReport
 	{
+		/// <summary>
+		/// One entry in the meta report. Contains a reference to a card and verious "meta statistics" linked to that card.
+		/// </summary>
 		public class MetaReportEntry
 		{
-			public string Name { get; set; }
-			public double DeckPercentage { get; set; }
-			public double CopiesNeededForAverage { get; set; }
-			public int TotalCopiesNeeded { get; set; }
-			public double Dominance { get; set; }
-			public string MetaStats { get; set; }
-			public Card Card { get; set; }
+			/// <summary>
+			/// The name of the card this entry is for.
+			/// </summary>
+			public string Name { get; private set; }
+
+			/// <summary>
+			/// The percentage of deck archetypes that include this card.
+			/// </summary>
+			public double DeckPercentage { get; private set; }
+
+			/// <summary>
+			/// The number of copies of the card the player needs to collect to have the average number of copies of this card played.
+			/// </summary>
+			public double CopiesNeededForAverage { get; private set; }
+
+			/// <summary>
+			/// The number of copies of the card the player needs to collect to be able to build any "meta" deck that plays it.
+			/// </summary>
+			public int TotalCopiesNeeded { get; private set; }
+
+			/// <summary>
+			/// The card's "meta dominance" score. This is basically CopiesNeededForAverage * DeckPercentage.
+			/// </summary>
+			public double Dominance { get; private set; }
+
+			/// <summary>
+			/// A string representation of the card's "meta statistics", for easy Xaml binding.
+			/// </summary>
+			public string MetaStats { get; private set; }
+
+			/// <summary>
+			/// A reference to the card that this entry is for.
+			/// </summary>
+			public Card Card { get; private set; }
+
+			/// <summary>
+			/// Constructor for meta report entries.
+			/// </summary>
+			/// <param name="name">The name of the card this entry is for.</param>
+			/// <param name="deckPercentage">The percentage of deck archetypes that include this card.</param>
+			/// <param name="copiesNeededForAverage">The number of copies of the card the player needs to collect to have the average number of copies of this card played.</param>
+			/// <param name="totalCopiesNeeded">The number of copies of the card the player needs to collect to be able to build any "meta" deck that plays it.</param>
+			/// <param name="dominance">The card's "meta dominance" score. This is basically CopiesNeededForAverage * DeckPercentage.</param>
+			/// <param name="metaStats">A string representation of the card's "meta statistics", for easy Xaml binding.</param>
+			/// <param name="card">A reference to the card that this entry is for.</param>
+			public MetaReportEntry(string name, double deckPercentage, double copiesNeededForAverage, int totalCopiesNeeded, double dominance, string metaStats, Card card)
+			{
+				Name = name;
+				DeckPercentage = deckPercentage;
+				CopiesNeededForAverage = copiesNeededForAverage;
+				TotalCopiesNeeded = totalCopiesNeeded;
+				Dominance = dominance;
+				MetaStats = metaStats;
+				Card = card;
+			}
 		}
 
+		/// <summary>
+		/// Gets a list of all of the entries in the report.
+		/// </summary>
 		public List<MetaReportEntry> ReportEntries { get; private set; }
 
+		/// <summary>
+		/// Gets the name of the next suggested set to purchase a booster for, for building toward the meta.
+		/// </summary>
 		public string NextBoosterSetToPurchase
 		{
 			get
@@ -48,27 +106,49 @@ namespace DailyArenaDeckAdvisor
 			}
 		}
 
+		/// <summary>
+		/// Gets a list of the top ten deck archetypes to focus on building in order to collect the top meta cards efficiently.
+		/// </summary>
 		public List<Archetype> TopDecksToBuild { get; private set; }
 
+		/// <summary>
+		/// A dictionary containing lists of card printings keyed by card name.
+		/// </summary>
 		private ReadOnlyDictionary<string, List<Card>> _cardsByName;
+
+		/// <summary>
+		/// A dictionary mapping card objects to their meta-statistics.
+		/// </summary>
 		private Dictionary<Card, CardStats> _cardStats;
+
+		/// <summary>
+		/// A list of all of the entries in the report.
+		/// </summary>
 		private List<MetaReportEntry> _allReportEntries;
 
+		/// <summary>
+		/// Constructor to generate a meta-report for a selected format.
+		/// </summary>
+		/// <param name="cardsByName">A dictionary containing lists of card printings keyed by card name.</param>
+		/// <param name="cardStats">A dictionary mapping card objects to their meta-statistics.</param>
+		/// <param name="cardsById">A dictioanry mapping Arena Ids to card objects.</param>
+		/// <param name="playerInventoryCounts">A dictionary containing card counts from the player's inventory, keyed by card name.</param>
+		/// <param name="archetypes">A list of deck archetypes for the format being computed.</param>
+		/// <param name="rotationProof">A boolean determining whether the user has the "Rotation" toggle turned on.</param>
 		public MetaReport(ReadOnlyDictionary<string, List<Card>> cardsByName, Dictionary<Card, CardStats> cardStats, ReadOnlyDictionary<int, Card> cardsById,
 			Dictionary<string, int> playerInventoryCounts, List<Archetype> archetypes, bool rotationProof)
 		{
 			_cardsByName = cardsByName;
 			_cardStats = cardStats;
-			_allReportEntries = cardsByName.Where(x => x.Value[0].Rarity != CardRarity.BasicLand && (!rotationProof || x.Value[0].Set.RotationSafe)).Select(x => new MetaReportEntry()
-			{
-				Name = x.Key,
-				DeckPercentage = _cardStats[x.Value[0]].DeckPercentage,
-				CopiesNeededForAverage = _cardStats[x.Value[0]].AverageCopies - Math.Min(playerInventoryCounts.ContainsKey(x.Key) ? playerInventoryCounts[x.Key] : 0, _cardStats[x.Value[0]].AverageCopies),
-				TotalCopiesNeeded = _cardStats[x.Value[0]].MaxCopies - Math.Min(playerInventoryCounts.ContainsKey(x.Key) ? playerInventoryCounts[x.Key] : 0, _cardStats[x.Value[0]].MaxCopies),
-				Dominance = _cardStats[x.Value[0]].DeckPercentage * (_cardStats[x.Value[0]].AverageCopies - Math.Min(playerInventoryCounts.ContainsKey(x.Key) ? playerInventoryCounts[x.Key] : 0, _cardStats[x.Value[0]].AverageCopies)),
-				MetaStats = _cardStats[x.Value[0]].MetaStatsView,
-				Card = x.Value[0]
-			}).Where(x => x.TotalCopiesNeeded > 0).ToList();
+			_allReportEntries = cardsByName.Where(x => x.Value[0].Rarity != CardRarity.BasicLand && (!rotationProof || x.Value[0].Set.RotationSafe)).Select(x => new MetaReportEntry(
+				name: x.Key,
+				deckPercentage: _cardStats[x.Value[0]].DeckPercentage,
+				copiesNeededForAverage: _cardStats[x.Value[0]].AverageCopies - Math.Min(playerInventoryCounts.ContainsKey(x.Key) ? playerInventoryCounts[x.Key] : 0, _cardStats[x.Value[0]].AverageCopies),
+				totalCopiesNeeded: _cardStats[x.Value[0]].MaxCopies - Math.Min(playerInventoryCounts.ContainsKey(x.Key) ? playerInventoryCounts[x.Key] : 0, _cardStats[x.Value[0]].MaxCopies),
+				dominance: _cardStats[x.Value[0]].DeckPercentage * (_cardStats[x.Value[0]].AverageCopies - Math.Min(playerInventoryCounts.ContainsKey(x.Key) ? playerInventoryCounts[x.Key] : 0, _cardStats[x.Value[0]].AverageCopies)),
+				metaStats: _cardStats[x.Value[0]].MetaStatsView,
+				card: x.Value[0]
+			)).Where(x => x.TotalCopiesNeeded > 0).ToList();
 
 			ReportEntries = new List<MetaReportEntry>(
 				_allReportEntries.OrderByDescending(x => x.Dominance).Take(70)
