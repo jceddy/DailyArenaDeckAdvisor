0a16cee6c0df88a17be2a2cf68ec828992b3ceda
Joseph Eddy <jceddy@hotmail.com>
2019-09-13 08:08:42 -0500

    Bumped version up to 1.0.3.2 for the next release. Added some code comments to the TabTemplateSelector class.

--- DailyArenaDeckAdvisor/TabTemplateSelector.cs
+++ DailyArenaDeckAdvisor/TabTemplateSelector.cs
@@ -1,15 +1,19 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
+﻿using System.Windows;
 using System.Windows.Controls;
 
 namespace DailyArenaDeckAdvisor
 {
+	/// <summary>
+	/// Class that selects the template used for tab details (switches between templates for Meta Report vs. Archetype).
+	/// </summary>
 	public class TabTemplateSelector : DataTemplateSelector
 	{
+		/// <summary>
+		/// Select the data template to use for the item being displayed.
+		/// </summary>
+		/// <param name="item">The item being displayed.</param>
+		/// <param name="container">The item's container.</param>
+		/// <returns>ArchetypeTabTemplate if the item is an Archetype object, MetaReportTabTemplate otherwise.</returns>
 		public override DataTemplate SelectTemplate(object item, DependencyObject container)
 		{
 			Window win = Application.Current.MainWindow;
