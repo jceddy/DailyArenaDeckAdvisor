3dc33310d96559a0eed16484d2c258ef45f1f113
Joseph Eddy <jceddy@hotmail.com>
2019-09-19 08:29:59 -0500

    Added code comments to FileLogger.

--- DailyArenaDeckAdvisorUpdater/FileLogger.cs
+++ DailyArenaDeckAdvisorUpdater/FileLogger.cs
@@ -3,9 +3,24 @@ using System.IO;
 
 namespace DailyArenaDeckAdvisorUpdater
 {
+	/// <summary>
+	/// Lightweight file logger for the updater.
+	/// </summary>
+	/// <remarks>
+	/// Can't use the full logger class here because the updater may have to update it.
+	/// </remarks>
 	public static class FileLogger
 	{
+		/// <summary>
+		/// The path of the log file to write to.
+		/// </summary>
 		public static string FilePath { get; set; }
+
+		/// <summary>
+		/// Write a debug message to the log file.
+		/// </summary>
+		/// <param name="message">The message to write.</param>
+		/// <param name="arg">Arguments to include with the message.</param>
 		public static void Log(string message, params object[] arg)
 		{
 			using (StreamWriter streamWriter = new StreamWriter(FilePath, true))
@@ -13,6 +28,13 @@ namespace DailyArenaDeckAdvisorUpdater
 				streamWriter.WriteLine(message, arg);
 			}
 		}
+
+		/// <summary>
+		/// Write an exception message to the log file.
+		/// </summary>
+		/// <param name="e">The exception to log.</param>
+		/// <param name="message">The messate to write.</param>
+		/// <param name="arg">Arguments to include wiht the message.</param>
 		public static void Log(Exception e, string message, params object[] arg)
 		{
 			using (StreamWriter streamWriter = new StreamWriter(FilePath, true))
