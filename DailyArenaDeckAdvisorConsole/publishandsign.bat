86b96e368bedf4450e65c4dceb3fd0bc12ac0d2b
Joseph Eddy <jceddy@hotmail.com>
2019-11-15 08:19:30 -0600

    Changed publishandsign batch file to do release version instead of debug

--- DailyArenaDeckAdvisorConsole/publishandsign.bat
+++ DailyArenaDeckAdvisorConsole/publishandsign.bat
@@ -1,7 +1,7 @@
-dotnet publish -f netcoreapp2.2 -r win-x86 --self-contained false
-dotnet publish -f netcoreapp2.2 -r win-x64 --self-contained false
-dotnet publish -f netcoreapp2.2 -r osx-x64 --self-contained false
-"C:\Program Files (x86)\Windows Kits\8.1\bin\x86\signtool.exe"  sign  /t http://timestamp.comodoca.com /sha1 9CFAD3D9DCDE70DC56909D3927A0DD5CB7DA1A83 ".\bin\Debug\netcoreapp2.2\win-x86\DailyArena.DeckAdvisor.Console.exe"
-"C:\Program Files (x86)\Windows Kits\8.1\bin\x86\signtool.exe"  sign  /t http://timestamp.comodoca.com /sha1 9CFAD3D9DCDE70DC56909D3927A0DD5CB7DA1A83 ".\bin\Debug\netcoreapp2.2\win-x86\publish\DailyArena.DeckAdvisor.Console.exe"
-"C:\Program Files (x86)\Windows Kits\8.1\bin\x64\signtool.exe"  sign  /t http://timestamp.comodoca.com /sha1 9CFAD3D9DCDE70DC56909D3927A0DD5CB7DA1A83 ".\bin\Debug\netcoreapp2.2\win-x64\DailyArena.DeckAdvisor.Console.exe"
-"C:\Program Files (x86)\Windows Kits\8.1\bin\x64\signtool.exe"  sign  /t http://timestamp.comodoca.com /sha1 9CFAD3D9DCDE70DC56909D3927A0DD5CB7DA1A83 ".\bin\Debug\netcoreapp2.2\win-x64\publish\DailyArena.DeckAdvisor.Console.exe"
+dotnet publish -f netcoreapp2.2 -r win-x86 -c Release --self-contained false
+dotnet publish -f netcoreapp2.2 -r win-x64 -c Release --self-contained false
+dotnet publish -f netcoreapp2.2 -r osx-x64 -c Release --self-contained false
+"C:\Program Files (x86)\Windows Kits\8.1\bin\x86\signtool.exe"  sign  /t http://timestamp.comodoca.com /sha1 9CFAD3D9DCDE70DC56909D3927A0DD5CB7DA1A83 ".\bin\Release\netcoreapp2.2\win-x86\DailyArena.DeckAdvisor.Console.exe"
+"C:\Program Files (x86)\Windows Kits\8.1\bin\x86\signtool.exe"  sign  /t http://timestamp.comodoca.com /sha1 9CFAD3D9DCDE70DC56909D3927A0DD5CB7DA1A83 ".\bin\Release\netcoreapp2.2\win-x86\publish\DailyArena.DeckAdvisor.Console.exe"
+"C:\Program Files (x86)\Windows Kits\8.1\bin\x64\signtool.exe"  sign  /t http://timestamp.comodoca.com /sha1 9CFAD3D9DCDE70DC56909D3927A0DD5CB7DA1A83 ".\bin\Release\netcoreapp2.2\win-x64\DailyArena.DeckAdvisor.Console.exe"
+"C:\Program Files (x86)\Windows Kits\8.1\bin\x64\signtool.exe"  sign  /t http://timestamp.comodoca.com /sha1 9CFAD3D9DCDE70DC56909D3927A0DD5CB7DA1A83 ".\bin\Release\netcoreapp2.2\win-x64\publish\DailyArena.DeckAdvisor.Console.exe"
